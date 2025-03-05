1d854a4e603dee97e50f638476d67f65830e75f6
Đoàn Thành Cao Nguyên <2151120042@ut.edu.vn>
2025-03-05 21:41:48 +0700

    Organize Folders

---
+++ Assets/_Core/_Scripts/DanceScene.cs
@@ -0,0 +1,266 @@
+using System;
+using System.Collections;
+using Sirenix.OdinInspector.Editor.Validation;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+
+[Serializable]
+public class TournamentTitle{
+    public string name;
+    public Color textColor;
+}
+public class DanceScene : MonoBehaviour
+{
+	[SerializeField] private SceneReference bonusScene;
+    [SerializeField] Renderer[] renderers;
+    [SerializeField] Material winMat;
+    [SerializeField] Material loseMat;
+    [SerializeField] float duration;
+    
+    [SerializeField] Transform[] characters;
+    [SerializeField] RuntimeAnimatorController idle;
+    [SerializeField] RenderTexture[] characterTextures;
+    [SerializeField] RawImage[] images;
+	
+    [SerializeField] Color winFog;
+    [SerializeField] Color loseFog;
+	
+    [SerializeField] Text title;
+    [SerializeField] Animator titleAnim;
+	
+    [SerializeField] SceneReference nextScene;
+	
+    [HideInInspector]
+    [SerializeField] Animator player;
+	
+    [HideInInspector]
+    [SerializeField] Animator opponent;
+	
+    [SerializeField] Text score;
+    [SerializeField] Image bar;
+	
+    [SerializeField] AudioSource winAudio;
+    [SerializeField] AudioSource loseAudio;
+	
+    [SerializeField] TextMeshProUGUI bonusLabel;
+	
+    [SerializeField] Animator mainPanel;
+    [SerializeField] GameObject tournamentInfo;
+	
+    [SerializeField] TextMeshProUGUI tournamentTitle;
+    [SerializeField] TournamentTitle[] tournamentNames;
+    [SerializeField] TextMeshProUGUI tournamentSmallLabel;
+	
+    [SerializeField] Transform playerIndicator;
+    [SerializeField] Transform[] targets;
+    [SerializeField] float indicatorSpeed;
+	
+    [SerializeField] float revealDelay;
+    [SerializeField] float tournamentPanelDuration;
+	
+    [SerializeField] Animator transition;
+	
+    [SerializeField] CanvasGroup tournamentGroup;
+    [SerializeField] CanvasGroup cupGroup;
+	
+    [SerializeField] GameObject bonusButton;
+    [SerializeField] GameObject playerPrefab;
+    [SerializeField]GameObject opponentPrefab;
+    [SerializeField] private MatchInfo info;
+	
+    Vector3 indicatorTarget;
+    bool moveIndicator;
+	
+    bool won = true;
+    bool wonTournament;
+    
+    
+    void Awake(){
+#if UNITY_ADS
+		TryAd();
+#endif
+	    tournamentInfo.SetActive(false);
+	    bonusButton.SetActive(false);
+		
+	    if(info == null)
+		    return;
+		
+	    won = info.Won;
+	    score.text = info.ScoreText;
+	    info.Reset();
+    }
+
+    private void Start() {
+	    bar.fillAmount = 0;
+	    if(won) {
+		    RenderSettings.fogColor = winFog;
+		    if(Camera.main != null) Camera.main.backgroundColor = winFog;
+		    title.text = "Winner";
+		    player.SetInteger("Type", Random.Range(1, 8));
+		    int tournament = PlayerPrefs.GetInt("Tournament");
+		    int tournamentMatch = PlayerPrefs.GetInt("Tournament Match Number",0);
+		    if(tournamentMatch == 0)
+			    SetCharacterData();
+		    LoadCharacters();
+		    
+		    int tournamentRange = tournament % tournamentNames.Length;
+			
+		    tournamentTitle.text = tournamentNames[tournamentRange].name;
+		    tournamentTitle.color = tournamentNames[tournamentRange].textColor;
+		    
+		    Color shadowColor = tournamentNames[tournamentRange].textColor;
+		    shadowColor.a = 0.5f;
+		    tournamentTitle.gameObject.GetComponent<Shadow>().effectColor = shadowColor;
+		    tournamentSmallLabel.text = "Tournament #" + (tournament + 1);
+		    
+		    PlayerPrefs.SetInt("Match", PlayerPrefs.GetInt("Match") + 1);
+		    PlayerPrefs.SetInt("Tournament Match Number", PlayerPrefs.GetInt("Tournament Match Number") + 1);
+			
+		    if(PlayerPrefs.GetInt("Tournament Match Number") == 3){
+			    PlayerPrefs.SetInt("Tournament Match Number", 0);
+			    PlayerPrefs.SetInt("Tournament", tournament + 1);
+				
+			    wonTournament = true;
+		    }
+			
+		    winAudio.Play();
+	    } else {
+		    RenderSettings.fogColor = loseFog;
+		    Camera.main.backgroundColor = loseFog;
+			
+		    title.text = "Keep trying!";
+			
+		    opponent.SetInteger("Type", Random.Range(1, 8));
+			
+		    int tournamentMatchNumber = PlayerPrefs.GetInt("Tournament Match Number");
+		    PlayerPrefs.SetInt("Tournament Match Number", 0);
+			
+		    PlayerPrefs.SetInt("Match", PlayerPrefs.GetInt("Match") - tournamentMatchNumber);
+			
+		    loseAudio.Play();
+	    }
+    }
+#if UNITY_ADS
+	void TryAd(){
+		AdManager adManager = GameObject.FindObjectOfType<AdManager>();
+		
+		if(adManager == null)
+			return;
+		
+		adManager.Interstitial();
+	}
+#endif
+    private void SetCharacterData() {
+	    PlayerPrefs.SetInt("Middle Layer Player Opponent", Random.Range(0, 2) + 2);
+	    PlayerPrefs.SetInt("Middle Layer Top Character", Random.Range(0, 2) + 4);
+	    PlayerPrefs.SetInt("Middle Layer Bottom Character", Random.Range(0, 2) + 6);
+	    PlayerPrefs.SetInt("Middle Layer Winner", Random.Range(0, 2));
+	    
+	    int match = PlayerPrefs.GetInt("Match");
+	    PlayerPrefs.SetInt("Opponent 1", match);
+	    
+	    int randomCharacter = Random.Range(0, 200);
+	    int middleOpponent = PlayerPrefs.GetInt("Middle Layer Player Opponent");
+	    PlayerPrefs.SetInt("Opponent 2", middleOpponent == 2 ? match + 1 : randomCharacter);
+	    PlayerPrefs.SetInt("Opponent 3", middleOpponent == 3 ? match + 1 : randomCharacter);
+	    int middleLayerWinnerTop = PlayerPrefs.GetInt("Middle Layer Winner") == 0 ? PlayerPrefs.GetInt("Middle Layer Top Character") : PlayerPrefs.GetInt("Middle Layer Bottom Character");
+	    
+	    for(int i = 4; i < 8; i++){
+		    PlayerPrefs.SetInt("Opponent " + i, Random.Range(0, 200));
+	    }
+	    PlayerPrefs.SetInt("Opponent " + middleLayerWinnerTop, match + 2);
+	    
+    }
+    void LoadCharacters(){
+	    GameObject playerCharacter = Instantiate(playerPrefab, characters[0].position, characters[0].rotation);
+	    playerCharacter.GetComponent<Player>().enabled = false;
+	    playerCharacter.GetComponent<Animator>().runtimeAnimatorController = idle;
+	    
+	    for(int i = 1; i < 8; i++){
+		    int index = PlayerPrefs.GetInt("Opponent " + i);
+			
+		    GameObject opponentCharacter = Instantiate(opponentPrefab, characters[i].position, characters[i].rotation);
+		    //opponentCharacter.GetComponent<ModifyOutfit>().Initialize(index);
+		    opponentCharacter.GetComponent<Opponent>().enabled = false;
+		    opponentCharacter.GetComponent<Animator>().runtimeAnimatorController = idle;
+	    }
+	}
+	IEnumerator AssignTextures(int tournamentMatch){
+		int middleLayerTop = PlayerPrefs.GetInt("Middle Layer Top Character");
+		int middleLayerBottom = PlayerPrefs.GetInt("Middle Layer Bottom Character");
+		
+		int targetIndex = tournamentMatch > 0 ? 2 : 1;
+		indicatorTarget = targets[targetIndex].position;
+		playerIndicator.position = targets[targetIndex - 1].position;
+		
+		if(wonTournament)
+			playerIndicator.position = targets[2].position;
+		
+		float delay = tournamentMatch > 0 ? 0f : revealDelay;
+		
+		yield return new WaitForSeconds(delay * 2f);
+		images[0].texture = characterTextures[0];
+			
+		yield return new WaitForSeconds(delay);
+		images[1].texture = characterTextures[PlayerPrefs.GetInt("Middle Layer Player Opponent")];
+			
+		yield return new WaitForSeconds(delay);
+		images[2].texture = characterTextures[middleLayerTop];
+			
+		yield return new WaitForSeconds(delay);
+		images[3].texture = characterTextures[middleLayerBottom];
+		
+		if(tournamentMatch > 0){
+			//player character image
+			yield return new WaitForSeconds(revealDelay);
+			images[4].texture = characterTextures[0];
+			
+			yield return new WaitForSeconds(revealDelay);
+			int bottomMostTexture = PlayerPrefs.GetInt("Middle Layer Winner") == 0 ? middleLayerTop : middleLayerBottom;
+			images[5].texture = characterTextures[bottomMostTexture];
+		}
+	}
+	IEnumerator Continue(){
+		if(!won){			
+			SceneManager.LoadScene(nextScene.Path);
+			
+			yield break;
+		}
+		
+		tournamentInfo.SetActive(true);
+		mainPanel.SetTrigger("Fade");
+		
+		if(!wonTournament){
+			StartCoroutine(AssignTextures(PlayerPrefs.GetInt("Tournament Match Number") - 1));
+			
+			yield return new WaitForSeconds(revealDelay * 5f);
+		
+			moveIndicator = true;
+		}
+		else{
+			tournamentGroup.alpha = 0;
+			cupGroup.alpha = 1;
+			
+			bonusButton.SetActive(true);
+		}
+	}
+	
+	//load the bonus scene
+	public void LoadBonus(){
+		StartCoroutine(LoadScene(true));
+	}
+	
+	//load a scene (bonus scene or next scene)
+	IEnumerator LoadScene(bool bonus){
+		transition.SetTrigger("Transition");
+		
+		yield return new WaitForSeconds(1f/4f);
+		
+		SceneManager.LoadScene(bonus ? bonusScene.Path : nextScene.Path);
+	}
+    
+}
